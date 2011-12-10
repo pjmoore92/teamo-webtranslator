6aea4a2a4459b6d18f05205987d0e121c7627597
alcoheca@gmail.com <alcoheca@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2011-12-10 13:18:38 +0000

    added: template support added: basic static site, (no backend pages yet)

---
+++ trunk/application/views/contact.php
@@ -0,0 +1,54 @@
+<?php if ( ! defined('BASEPATH')) exit('Access denied'); ?>
+
+<div class="content">
+<div class="page-header">
+  <h1>Bethel Translations <small>Contact page</small></h1>
+</div>
+<div class="row">
+<br />
+<br />
+<br />
+  <div class="span10 offset2">
+        <form>
+                <fieldset>
+                  <legend>Contact form</legend>
+                  <div class="clearfix">
+                    <label for="xlInput">X-Large input</label>
+                    <div class="input">
+                      <input class="xlarge" id="xlInput" name="xlInput" size="30" type="text">
+                    </div>
+                  </div><!-- /clearfix -->
+
+                  <div class="clearfix">
+                    <label for="xlInput">X-Large input</label>
+                    <div class="input">
+                      <input class="xlarge" id="xlInput" name="xlInput" size="30" type="text">
+                    </div>
+                  </div><!-- /clearfix -->
+
+                  <div class="clearfix">
+                    <label for="xlInput">X-Large input</label>
+                    <div class="input">
+                      <input class="xlarge" id="xlInput" name="xlInput" size="30" type="text">
+                    </div>
+                  </div><!-- /clearfix -->
+
+                  <div class="clearfix">
+                    <label for="textarea">Textarea</label>
+                    <div class="input">
+                      <textarea class="xxlarge" id="textarea2" name="textarea2" rows="10"></textarea>
+                      <span class="help-block">
+                        Block of help text to describe the field above if need be.
+                      </span>
+                    </div>
+                  </div><!-- /clearfix -->
+
+                  <div class="actions">
+                    <input type="submit" class="btn primary" value="Send e-mail">&nbsp;<button type="reset" class="btn">Cancel</button>
+                  </div>
+                </fieldset>
+        </form>	
+  </div>
+</div>
+</div>
+
