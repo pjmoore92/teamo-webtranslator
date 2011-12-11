5fac2236cf9812a6fd4a5e284f17a4818ac8b1e7
andrei.mustata <andrei.mustata@gmail.com>
2011-12-11 17:48:53 +0000

    added TankAuth files

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'team_project';
+$db['default']['password'] = 's5m4peNqJxwQTUvH';
+$db['default']['database'] = 'team_project';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -40,6 +40,25 @@ class Welcome extends CI_Controller {
                 $this->template->set('title', 'Contact -');
                 $this->template->load('template', 'contact');
 	}
+
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('url');
+		$this->load->library('tank_auth');
+	}
+
+	function index()
+	{
+		if (!$this->tank_auth->is_logged_in()) {
+			redirect('/auth/login/');
+		} else {
+			$data['user_id']	= $this->tank_auth->get_user_id();
+			$data['username']	= $this->tank_auth->get_username();
+			$this->load->view('welcome', $data);
+		}
+	}
 }
 
 /* End of file welcome.php */
