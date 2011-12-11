b8ba2aea4e3ff560daf0250957bed87a22cdb9c2
andrei.mustata@gmail.com <andrei.mustata@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2011-12-11 17:48:53 +0000

    added TankAuth files

--- branches/tankauth/application/config/config.php
+++ branches/tankauth/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://server/';
+$config['base_url']	= 'http://localhost/teamo/tankauth/';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'mkLB2ZlNM0i2F9lLfYqr';
 
 /*
 |--------------------------------------------------------------------------
@@ -248,7 +248,7 @@ $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
-$config['sess_use_database']	= FALSE;
+$config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
@@ -266,7 +266,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "";
-$config['cookie_domain']	= ".alasdaircampbell.com";
+$config['cookie_domain']	= ".localhost";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
