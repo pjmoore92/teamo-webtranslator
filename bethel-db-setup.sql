c3fe0d16d3292263e0245529a7842a5d9564036b
alcoheca <alcoheca@gmail.com>
2011-12-12 18:44:21 +0000

    changed: use InnoDB engine

--- bethel-db-setup.sql
+++ bethel-db-setup.sql
@@ -2,9 +2,6 @@
 -- USE TO CREATE A FRESH SET OF TABLES 
 --
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
 --
 -- Database: `claddach_igni348`
 --
@@ -15,14 +12,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `customer` (
-  `customerID` int(11) NOT NULL auto_increment,
+  `customerID` int(11) NOT NULL AUTO_INCREMENT,
   `fullName` text NOT NULL,
   `title` enum('Mr','Mrs','Ms') NOT NULL,
   `email` text NOT NULL,
   `active` tinyint(1) NOT NULL default '0',
   `referenceStr` varchar(8) NOT NULL,
   PRIMARY KEY  (`customerID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 -- --------------------------------------------------------
 --
@@ -30,10 +27,10 @@ CREATE TABLE IF NOT EXISTS `customer` (
 --
 
 CREATE TABLE IF NOT EXISTS `document` (
-  `documentID` int(11) NOT NULL auto_increment,
+  `documentID` int(11) NOT NULL AUTO_INCREMENT,
   `filePath` varchar(256) NOT NULL,
   PRIMARY KEY  (`documentID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 -- --------------------------------------------------------
 --
@@ -41,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `document` (
 --
 
 CREATE TABLE IF NOT EXISTS `jobs` (
-  `jobID` int(11) NOT NULL auto_increment,
+  `jobID` int(11) NOT NULL AUTO_INCREMENT,
   `customerID` int(11) NOT NULL,
   `status` enum('QuoteReq','QuoteSent','QuoteAccept','QuoteDeclined','Paid','Translated','Complete') NOT NULL,
   `quote` int(10) NOT NULL,
@@ -52,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `urgent` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`jobID`),
   KEY `idx_jobs` (`customerID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 ALTER TABLE jobs
 ADD FOREIGN KEY (customerID) REFERENCES customer(customerID);
@@ -63,7 +60,7 @@ ADD FOREIGN KEY (customerID) REFERENCES customer(customerID);
 --
 
 CREATE TABLE IF NOT EXISTS `translation` (
-  `translationID` int(11) NOT NULL auto_increment,
+  `translationID` int(11) NOT NULL AUTO_INCREMENT,
   `jobID` int(11) NOT NULL,
   `origDoc` int(11) NOT NULL,
   `translatedDoc` int(11) NOT NULL,
@@ -71,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `translation` (
   KEY `idx_translation` (`jobID`),
   KEY `idx_translation_1` (`origDoc`),
   KEY `idx_translation_2` (`translatedDoc`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 ALTER TABLE translation 
 ADD FOREIGN KEY (jobID) REFERENCES jobs(jobID);
@@ -82,13 +79,13 @@ ADD FOREIGN KEY (jobID) REFERENCES jobs(jobID);
 --
 
 CREATE TABLE IF NOT EXISTS `note` (
-  `noteID` int(11) NOT NULL auto_increment,
+  `noteID` int(11) NOT NULL AUTO_INCREMENT,
   `jobID` int(11) NOT NULL,
   `created` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `text` text NOT NULL,
   PRIMARY KEY (`noteID`),
   KEY `idx_note` (`jobID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 ALTER TABLE note
 ADD FOREIGN KEY (jobID) REFERENCES jobs(jobID);
