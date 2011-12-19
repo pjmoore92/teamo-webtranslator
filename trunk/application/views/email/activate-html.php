3d2c89085523ccdfcbd0ac7f699b04fc7433a083
andrei.mustata@gmail.com <andrei.mustata@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2011-12-19 16:16:21 +0000

    fixed: now the activation email displays the reference code

--- trunk/application/views/email/activate-html.php
+++ trunk/application/views/email/activate-html.php
@@ -11,17 +11,17 @@
 Thanks for joining <?php echo $site_name; ?>. We listed your sign in details below, make sure you keep them safe.<br />
 To verify your email address, please follow this link:<br />
 <br />
-<big style="font: 16px/18px Arial, Helvetica, sans-serif;"><b><a href="<?php echo site_url('/auth/activate/'.$user_id.'/'.$new_email_key); ?>" style="color: #3366cc;">Finish your registration...</a></b></big><br />
+<big style="font: 16px/18px Arial, Helvetica, sans-serif;"><b><a href="<?php echo site_url('auth/activate/'.$user_id.'/'.$new_email_key); ?>" style="color: #3366cc;">Finish your registration...</a></b></big><br />
 <br />
 Link doesn't work? Copy the following link to your browser address bar:<br />
-<nobr><a href="<?php echo site_url('/auth/activate/'.$user_id.'/'.$new_email_key); ?>" style="color: #3366cc;"><?php echo site_url('/auth/activate/'.$user_id.'/'.$new_email_key); ?></a></nobr><br />
+<nobr><a href="<?php echo site_url('auth/activate/'.$user_id.'/'.$new_email_key); ?>" style="color: #3366cc;"><?php echo site_url('auth/activate/'.$user_id.'/'.$new_email_key); ?></a></nobr><br />
 <br />
 Please verify your email within <?php echo $activation_period; ?> hours, otherwise your registration will become invalid and you will have to register again.<br />
 <br />
 <br />
 <?php if (strlen($username) > 0) { ?>Your username: <?php echo $username; ?><br /><?php } ?>
 Your email address: <?php echo $email; ?><br />
-<?php if (isset($password)) { /* ?>Your password: <?php echo $password; ?><br /><?php */ } ?>
+<?php if (isset($password)) {  ?>Your client reference number: <?php echo $password; ?><br /><?php  } ?>
 <br />
 <br />
 Have fun!<br />
