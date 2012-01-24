eff622af6ad8a80df1b3ebd4a876ce2d58775af4
andrei.mustata <andrei.mustata@gmail.com>
2012-01-24 18:06:13 +0000

    updated table `job`; udpated table `translation`; removed table `notes`; updated table `document`; updated table `customer`

--- bethel-db-setup.sql
+++ bethel-db-setup.sql
@@ -15,11 +15,8 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `customerID` int(11) NOT NULL AUTO_INCREMENT,
   `fullName` text NOT NULL,
   `title` enum('Mr','Mrs','Ms') NOT NULL,
-  `email` text NOT NULL,
-  `active` tinyint(1) NOT NULL default '0',
-  `referenceStr` varchar(8) NOT NULL,
   PRIMARY KEY  (`customerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 --
@@ -30,29 +27,25 @@ CREATE TABLE IF NOT EXISTS `document` (
   `documentID` int(11) NOT NULL AUTO_INCREMENT,
   `filePath` varchar(256) NOT NULL,
   PRIMARY KEY  (`documentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 --
 -- Table structure for table `jobs`
 --
 
-CREATE TABLE IF NOT EXISTS `jobs` (
-  `jobID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `job` (
+  `jobID` int(11) NOT NULL auto_increment,
   `customerID` int(11) NOT NULL,
   `status` enum('QuoteReq','QuoteSent','QuoteAccept','QuoteDeclined','Paid','Translated','Complete') NOT NULL,
-  `quote` int(10) NOT NULL,
+  `quote` int(10) default NULL,
   `dateRequested` date NOT NULL,
   `dateDue` date NOT NULL,
   `fromLanguage` text NOT NULL,
   `toLanguage` text NOT NULL,
-  `urgent` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`jobID`),
-  KEY `idx_jobs` (`customerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
-ALTER TABLE jobs
-ADD FOREIGN KEY (customerID) REFERENCES customer(customerID);
+   PRIMARY KEY  (`jobID`),
+   FOREIGN KEY (`customerID`) REFERENCES users (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 --
@@ -60,36 +53,14 @@ ADD FOREIGN KEY (customerID) REFERENCES customer(customerID);
 --
 
 CREATE TABLE IF NOT EXISTS `translation` (
-  `translationID` int(11) NOT NULL AUTO_INCREMENT,
+  `translationID` int(11) NOT NULL auto_increment,
   `jobID` int(11) NOT NULL,
   `origDoc` int(11) NOT NULL,
-  `translatedDoc` int(11) NOT NULL,
+  `translatedDoc` int(11) default NULL,
   PRIMARY KEY  (`translationID`),
-  KEY `idx_translation` (`jobID`),
-  KEY `idx_translation_1` (`origDoc`),
-  KEY `idx_translation_2` (`translatedDoc`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-ALTER TABLE translation 
-ADD FOREIGN KEY (jobID) REFERENCES jobs(jobID);
-
--- --------------------------------------------------------
---
--- Table structure for table `notes`
---
-
-CREATE TABLE IF NOT EXISTS `note` (
-  `noteID` int(11) NOT NULL AUTO_INCREMENT,
-  `jobID` int(11) NOT NULL,
-  `created` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `text` text NOT NULL,
-  `starred` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY (`noteID`),
-  KEY `idx_note` (`jobID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-ALTER TABLE note
-ADD FOREIGN KEY (jobID) REFERENCES jobs(jobID);
+  FOREIGN KEY `origDoc` REFERENCES `document` (`documentID`),
+  FOREIGN KEY `translatedDoc` REFERENCES `document` (`documentID`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
 -- ------------- TankAuth tables -------------
