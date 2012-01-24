e879cba0a1c7e95b70e9d9e53667bf10277268ed
s.hayton91@gmail.com <s.hayton91@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-01-24 20:30:28 +0000

    Added models for quote sent, awaiting payment, translations and history.

--- trunk/application/models/job_model.php
+++ trunk/application/models/job_model.php
@@ -10,14 +10,53 @@ class Job_model extends CI_Model{
 
 
     public function get_all_pending($customerID){
-    	$this->db->where('status', 'QuoteReq');
+    	$this->db->where('status', 'QuoteReq');       
     	$this->db->where('customerID', $customerID);
     	$query = $this->db->get($this->_table);
 		
     	if($query->num_rows() > 0) return $query->result();
     	return NULL;
     }
-
+    
+    public function get_all_quoted($customerID){
+      $this->db->where('status', 'QuoteSent');        
+      $this->db->where('customerID', $customerID);
+      $query = $this->db->get($this->_table);
+		
+    	if($query->num_rows() > 0) return $query->result();
+    	return NULL;
+    }
+    
+    public function get_all_paid($customerID){
+      $this->db->where('status', 'Paid');        
+      $this->db->where('customerID', $customerID);
+      $query = $this->db->get($this->_table);
+		
+    	if($query->num_rows() > 0) return $query->result();
+    	return NULL;
+    }
+     
+    public function get_all_translated($customerID){
+      $this->db->where('status', 'Translated');        
+      $this->db->where('customerID', $customerID);
+      $query = $this->db->get($this->_table);
+		
+    	if($query->num_rows() > 0) return $query->result();
+    	return NULL;
+    }     
+    
+    public function get_all_history($customerID){
+      $cur_date = getdate();
+      $due_date = $this->db->get('dateDue');
+      $interval = $cur_date->diff($due_date);
+      
+      $this->db->where('status', 'Translated');        
+      $this->db->where('customerID', $customerID);
+      $query = $this->db->get($this->_table);
+      /* Assumes completed jobs are held in translations for 90 days, then moved to history after that period.*/
+    	if($query->num_rows() > 0 && $interval->d >90 ) return $query->result();
+    	return NULL;
+    }
 }
 
 /* End of file job_model.php */
