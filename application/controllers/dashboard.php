62a0be68df8561b145e33c461ffe1caf6c266719
alcoheca <alcoheca@gmail.com>
2012-01-31 10:48:27 +0000

    refactor job models and dashboard calls

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -2,134 +2,107 @@
 
 class Dashboard extends MY_Controller {
 
-	// private $_user_id;
-	// private $_client_name;
-	// private $_view_template;
+    // private $_user_id;
+    // private $_client_name;
+    // private $_view_template;
 
-	public function __construct(){
-		parent::__construct();
+    public function __construct(){
+        parent::__construct();
 
-		// $this->load->library('tank_auth');
+        // $this->load->library('tank_auth');
 
-		// if(!$this->tank_auth->is_logged_in())
-		// 	redirect('auth/login');
-		// else{
-		// 	$this->_user_id = $this->session->userdata('user_id');
+        // if(!$this->tank_auth->is_logged_in())
+        // 	redirect('auth/login');
+        // else{
+        // 	$this->_user_id = $this->session->userdata('user_id');
 
-		// 	$this->load->model('customer_model');
-		// 	$user = $this->customer_model->get_customer_name($this->_user_id);
-		// 	$this->_client_name = $user->title . '. ' . $user->fullName;
-		// 	//$this->session->userdata('role');
+        // 	$this->load->model('customer_model');
+        // 	$user = $this->customer_model->get_customer_name($this->_user_id);
+        // 	$this->_client_name = $user->title . '. ' . $user->fullName;
+        // 	//$this->session->userdata('role');
 
-		// 	$this->_view_template = 'dashboard/client/template';
-		// }
-	}
+        // 	$this->_view_template = 'dashboard/client/template';
+        // }
+    }
 
-	public function index(){
+    public function index(){
 
-		$this->_data['content'] = 'dashboard/client/main';
+        $this->_data['content'] = 'dashboard/client/main';
 
-		$this->template->set('title', ucfirst($this->_data['role']).' Dashboard -');
-		$this->template->load('template', $this->_view_template, $this->_data);
-	}
+        $this->template->set('title', ucfirst($this->_data['role']).' Dashboard -');
+        $this->template->load('template', $this->_view_template, $this->_data);
+    }
 
-	public function submit(){
+    public function submit(){
 
-		$this->lang->load('common');
-		$this->lang->load('home');
+        $this->lang->load('common');
+        $this->lang->load('home');
 
 
-		$_data['content'] = 'dashboard/client/submit';
+        $_data['content'] = 'dashboard/client/submit';
 
-		$this->template->set('title', ucfirst($_data['role']).' Dashboard -');
-		$this->template->load('template', $this->_view_template, $_data);
-	}
+        $this->template->set('title', ucfirst($_data['role']).' Dashboard -');
+        $this->template->load('template', $this->_view_template, $_data);
+    }
 
-	public function add_job(){
+    public function add_job(){
 
-		if(!$this->input->is_ajax_request()){
-			return json_encode(array('error' => 'Somthin\' ain\' right :<'));
-		}
-		else{
-			
-			$this->load->library('form_validation');
-			
-			$this->form_validation->set_rules('toLanguage', 'To language', 'required');
-			$this->form_validation->set_rules('fromLanguage', 'From language', 'required');
+        if(!$this->input->is_ajax_request()){
+            return json_encode(array('error' => 'Somthin\' ain\' right :<'));
+        }
+        else{
 
-			if ($this->form_validation->run() == FALSE){
-				return json_encode(array('error' => 'Somthin\' ain\' right :<'));
-			}
-			else{
-				
-				$dateDue = date('Y-m-d H:i:s', time() + (7 * 24 * 60 * 60));
+            $this->load->library('form_validation');
 
-				$job_data = array(
-								'customerID' => $this->_user_id,
-								'status' => 'QuoteReq',
-								'dateDue' => $dateDue,
-								'toLanguage' => $this->form_validation->set_value('toLanguage'),
-								'fromLanguage' => $this->form_validation->set_value('fromLanguage')
-							);
+            $this->form_validation->set_rules('toLanguage', 'To language', 'required');
+            $this->form_validation->set_rules('fromLanguage', 'From language', 'required');
 
-				$this->load->model('job_model');
-				$this->job_model->add_job($job_data);
+            if ($this->form_validation->run() == FALSE){
+                return json_encode(array('error' => 'Somthin\' ain\' right :<'));
+            }
+            else{
 
-				die(json_encode(array('response' => 'WOOHOO!')));
-			}
-		}
+                $dateDue = date('Y-m-d H:i:s', time() + (7 * 24 * 60 * 60));
 
-		// $data['name'] = $this->_client_name;
-		// $data['content'] = 'dashboard/client/main';
+                $job_data = array(
+                    'customerID' => $this->_user_id,
+                    'status' => 'QuoteReq',
+                    'dateDue' => $dateDue,
+                    'toLanguage' => $this->form_validation->set_value('toLanguage'),
+                    'fromLanguage' => $this->form_validation->set_value('fromLanguage')
+                );
 
-		// $this->template->set('title', 'Client Dashboard -');
-		// $this->template->load('template', $this->_view_template, $data);
-		
+                $this->load->model('job_model');
+                $this->job_model->add_job($job_data);
 
-	}
+                die(json_encode(array('response' => 'WOOHOO!')));
+            }
+        }
 
-	public function pending(){
-		
-		$this->load->model('job_model');
-		$this->_data['pending_jobs_list'] = $this->job_model->get_all_pending($this->_user_id);
+        // $data['name'] = $this->_client_name;
+        // $data['content'] = 'dashboard/client/main';
 
-		$this->_data['content'] = "dashboard/{$this->_data['role']}/pending";
+        // $this->template->set('title', 'Client Dashboard -');
+        // $this->template->load('template', $this->_view_template, $data);
 
-		$this->template->set('title', $this->_data['role']. ' Dashboard -');
-		$this->template->load('template', $this->_view_template, $this->_data);
-	}
-   
-  public function quotes(){
-  
-		$this->load->model('job_model');
-		$this->_data['quoted_jobs_list'] = $this->job_model->get_all_quoted($this->_user_id);
 
-		$this->_data['content'] = "dashboard/{$this->_data['role']}/quotes";
+    }
 
-		$this->template->set('title', $this->_data['role']. ' Dashboard -');
-		$this->template->load('template', $this->_view_template, $this->_data);
-  }
-  
-  public function translations(){
+    private function _retrieveData(String statusType) {
+        $this->load->model('job_model');
 
-		$this->load->model('job_model');
-		$this->_data['translated_jobs_list'] = $this->job_model->get_all_translated($this->_user_id);
+        $this->_data['jobs_list'] = $this->job_model->get_by_status(statusType, $this->_user_id);
+        $this->_data['content'] = "dashboard/{$this->_data['role']}/"+status;
+        $this->template->set('title', $this->_data['role']. ' Dashboard -');
+        $this->template->load('template', $this->_view_template, $this->_data);
 
-		$this->_data['content'] = "dashboard/{$this->_data['role']}/translations";
+    }
+    public function pending(){ $this->_retrieveData("pending"); }
+    public function quotes(){ $this->_retrieveData("quotes"); }
+    public function translated(){ $this->_retrieveData("translated"); }
 
-		$this->template->set('title', $this->_data['role']. ' Dashboard -');
-		$this->template->load('template', $this->_view_template, $this->_data);
-  }
-  
-  public function history(){
-  
-		$this->load->model('job_model');
-		$this->_data['historic_jobs_list'] = $this->job_model->get_all_history($this->_user_id);
-
-		$this->_data['content'] = "dashboard/{$this->_data['role']}/history";
-
-		$this->template->set('title', $this->_data['role']. ' Dashboard -');
-		$this->template->load('template', $this->_view_template, $this->_data);
-  }
-  
+    public function history(){
+        //TODO fix historical translations
+        $this->_retrieveData("translated");
+    }
 }
--- application/models/job_model.php
+++ application/models/job_model.php
@@ -3,74 +3,49 @@
 class Job_model extends CI_Model{
 
     private $_table = 'job';
-    
+
     public function __construct(){
         parent::__construct();
     }
 
+    public function get_by_status(String status, $customerID =  NULL) {
+        $this->db->where('status', status);
+        if (customerID != NULL) $this->db->where('customerID', $customerID);
 
-    public function get_all_pending($customerID){
-    	$this->db->where('status', 'QuoteReq');       
-    	$this->db->where('customerID', $customerID);
-    	$query = $this->db->get($this->_table);
-		
-    	if($query->num_rows() > 0) return $query->result();
-    	return NULL;
+        $query = $this->db->get($this->_table);
+        if($query->num_rows() > 0)
+            return $query->result();
+        else
+            return NULL;
     }
-    
-    public function get_all_quoted($customerID){
-      $this->db->where('status', 'QuoteSent');        
-      $this->db->where('customerID', $customerID);
-      $query = $this->db->get($this->_table);
-		
-    	if($query->num_rows() > 0) return $query->result();
-    	return NULL;
-    }
-    
-    public function get_all_paid($customerID){
-      $this->db->where('status', 'Paid');        
-      $this->db->where('customerID', $customerID);
-      $query = $this->db->get($this->_table);
-		
-    	if($query->num_rows() > 0) return $query->result();
-    	return NULL;
-    }
-     
-    public function get_all_translated($customerID){
-      $this->db->where('status', 'Translated');        
-      $this->db->where('customerID', $customerID);
-      $query = $this->db->get($this->_table);
-		
-    	if($query->num_rows() > 0) return $query->result();
-    	return NULL;
-    }     
-    
+
     /*
       needs to check the date
-    */
-    public function get_all_history($customerID){
-      // $cur_date = getdate();
-      // $due_date = $this->db->get('dateDue');
-      // $interval = $cur_date->diff($due_date);
-      
-      $this->db->where('status', 'Translated');        
-      $this->db->where('customerID', $customerID);
-      $query = $this->db->get($this->_table);
-      /* Assumes completed jobs are held in translations for 90 days, then moved to history after that period.*/
-    	if($query->num_rows() > 0 /*&& $interval->d >90*/ ) return $query->result();
-    	return NULL;
+     */
+    public function get_all_history($customerID = NULL){
+        // $cur_date = getdate();
+        // $due_date = $this->db->get('dateDue');
+        // $interval = $cur_date->diff($due_date);
+
+        $this->db->where('status', 'Translated');        
+        if ($customerID != 0)
+            $this->db->where('customerID', $customerID);
+        $query = $this->db->get($this->_table);
+        /* Assumes completed jobs are held in translations for 90 days, then moved to history after that period.*/
+        if($query->num_rows() > 0 /*&& $interval->d >90*/ ) return $query->result();
+        return NULL;
     }
 
     public function add_job($job_data){
-      
-      $this->db->insert($this->_table, $job_data);
+
+        $this->db->insert($this->_table, $job_data);
 
     }
 
     public function update_job_dates($job_id, $job_dates){
-      
-      $this->db->where('jobID', $job_id);
-      $this->db->update($this->_table, $job_dates, FALSE);
+
+        $this->db->where('jobID', $job_id);
+        $this->db->update($this->_table, $job_dates, FALSE);
     }
 }
 
