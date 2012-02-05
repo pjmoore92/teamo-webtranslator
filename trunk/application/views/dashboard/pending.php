aaf378bbf060d5b3b7e01f2a250f7be17d1cf860
pjmoore92@gmail.com <pjmoore92@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-05 19:44:39 +0000

    changed accordion code.

--- trunk/application/views/dashboard/pending.php
+++ trunk/application/views/dashboard/pending.php
@@ -7,27 +7,28 @@
             <p><strong>Update:</strong> Joelle has sent you the quote for Docname3. Please review it!</p>
           </div>-->
 
-          <div class="alert-message block-message warning fade in" data-alert="alert" >
-            <a class="close" href="#">&times;</a>
+          <div class="alert alert-block alert-error fade in">
+            <a class="close" data-dismiss="alert" href="#">&times;</a>
             <p>
               <strong>Update:</strong> Joelle has sent you a quote for <strong>Docname2</strong>:
               It will cost &pound;50 and it will be ready on the <time>2011-09-17</time>.
             </p>
             <div class="alert-actions">
-              <a class="btn small success" href="#">I accept!</a>
-              <a class="btn small danger" href="#">No, thanks!</a>
+              <a class="btn btn-small btn-success" href="#">I accept!</a>
+              <a class="btn btn-small btn-danger" href="#">No, thanks!</a>
             </div>
           </div>
 
         </div>
         <div class="row">
-        <div class="span16">
+        <div class="span9">
           <?php
               if($jobs_list == NULL):
                 echo ':( You have no pending jobs.';
               else:
           ?>
-          <table class="zebra-striped" id="sortTableExample">
+		  <?php endif; ?>
+         <!-- <table class="zebra-striped" id="sortTableExample">
             <thead>
               <tr>
                 <th class="header headerSortDown">#</th>
@@ -36,16 +37,26 @@
                 <th class="blue header">From language</th>
                 <th class="blue header">To language</th>
               </tr>
-            </thead>
-            <?php foreach($jobs_list as $job): ?>
-            <tr>
-              <td><?php echo $job->jobID; ?></td>
-              <td><?php echo $job->dateRequested; ?></td>
-              <td><?php echo $job->dateDue; ?></td>
-              <td><?php echo $job->fromLanguage; ?></td>
-              <td><?php echo $job->toLanguage; ?></td>
-            </tr>
-            <?php endforeach; ?>
-          </table>
-          <?php endif; ?>
+            </thead>-->
+	    <div class="accordion" id="accordion2">
+	    <?php foreach($jobs_list as $job): ?>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+					<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#<?php echo $job->jobID; ?>"> Job <?php echo $job->jobID; ?></a>
+				</div>
+				<div id="<?php echo $job->jobID; ?>" class="accordion-body collapse in">
+					<div class="accordion-inner">
+					<tr>
+						<td><?php echo $job->jobID; ?></td>
+						<td><?php echo $job->dateRequested; ?></td>
+						<td><?php echo $job->dateDue; ?></td>
+						<td><?php echo $job->fromLanguage; ?></td>
+						<td><?php echo $job->toLanguage; ?></td>
+					</tr>
+					</div>
+				</div>
+				</div>
+		<?php endforeach; ?>
+        </div>
+		
         </div>
