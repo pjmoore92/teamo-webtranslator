22492e5826e7173727529c990dd995cb436722bd
pjmoore92 <pjmoore92@gmail.com>
2012-02-05 19:59:30 +0000

    pending now formatted correctly - still needs code to display documents for jobs

--- application/views/dashboard/pending.php
+++ application/views/dashboard/pending.php
@@ -46,13 +46,61 @@
 				</div>
 				<div id="<?php echo $job->jobID; ?>" class="accordion-body collapse in">
 					<div class="accordion-inner">
-					<tr>
-						<td><?php echo $job->jobID; ?></td>
-						<td><?php echo $job->dateRequested; ?></td>
-						<td><?php echo $job->dateDue; ?></td>
-						<td><?php echo $job->fromLanguage; ?></td>
-						<td><?php echo $job->toLanguage; ?></td>
-					</tr>
+						<div class="well">
+							<div class="row">
+								<div class="span3">
+									<dl>
+										<dt>Client</dt>
+											<dd><?php echo $client_name; ?></dd>
+										<dt>Languages</dt>
+											<dd><?php echo $job->fromLanguage; ?> to <?php echo $job->toLanguage; ?></dd>
+										<dt>Sent on</dt>
+											<dd><time><?php echo $job->dateRequested; ?></time></dd>
+									</dl>
+								</div>
+								<div class="span3">
+									<dl>
+										<dt>Quote</dt>
+											<dd>&mdash;</dd>
+										<dt>Date Due</dt>
+											<dd><?php echo $job->dateDue; ?></dt>
+									</dl>
+									<!--<button class="btn">Send the Quote</button>-->
+								</div>
+							</div>
+							  <table class="table table-striped" id="sortTableExample">
+								<thead>
+								  <tr>
+									<th class="header headerSortDown" width="20">#</th>
+									<th class="header">Document Name</th>
+									<th class="blue header">Word count</th>
+									<th class="blue header"><abbr title="Estimated time of arrival">ETA</abbr></th>
+									<th class="blue header">Quote</th>
+								  </tr>
+								</thead>
+								<tbody>
+								  <tr>
+									<td>1</td>
+									<td>Docname</td>
+									<td>9,000</td>
+									<td><time>2011-09-15</time></td>
+									<td>£100</td>
+								  </tr><tr>
+									<td>2</td>
+									<td>Docname2</td>
+									<td>9,000</td>
+									<td><time>2011-09-17</time></td>
+									<td>£70</td>
+								  </tr><tr>
+									<td>3</td>
+									<td>Docname3</td>
+									<td>9,000</td>
+									<td><time>2011-09-07</time></td>
+									<td>£50</td>
+								  </tr>
+								</tbody>
+							  </table>
+						</div><!-- end .well -->
 					</div>
 				</div>
 				</div>
