3fb3323ddeaa6b3948dffe286f2a1ec72a66d780
s.hayton91@gmail.com <s.hayton91@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-06 16:28:20 +0000

    Fixed some errors with welcome message

--- trunk/application/language/english/common_lang.php
+++ trunk/application/language/english/common_lang.php
@@ -2,11 +2,13 @@
 $lang['company.name'] = "Bethel Translations";
 $lang['Welcome'] = "Welcome";
 $lang['paragraph_p1'] = "I offer excellent translation services with a primary focus on translating documents 
-from French to English and vice versa. For more information on myself, please see "
-$lang['paragraph_p2'] = "I also offer the following services: "
-$lang['paragraph_p3'] = "and "
-$lang['paragraph_p4'] = "You can see what other people thought of my work "
+from French to English and vice versa. For more information on myself, please see ";
+$lang['paragraph_p2'] = "I also offer the following services: ";
+$lang['paragraph_p3'] = "and ";
+$lang['paragraph_p4'] = "You can see what other people thought of my work ";
 /**/
+$lang['nav.lang'] = "English";
+$lang['nav.serv'] = "Services";
 $lang['nav.home'] = "Home";
 $lang['nav.login'] = "Login";
 $lang['nav.about'] = "About";
