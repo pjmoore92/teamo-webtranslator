32453843437ef3185ff650b133e0ec8b805677a1
s.hayton91@gmail.com <s.hayton91@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-06 16:10:57 +0000

    Welcome message adapts to language changes (only english for now)

--- trunk/application/language/english/common_lang.php
+++ trunk/application/language/english/common_lang.php
@@ -1,6 +1,11 @@
 <?php
 $lang['company.name'] = "Bethel Translations";
-$lang['helloWorld'] = "Hello, world!";
+$lang['Welcome'] = "Welcome";
+$lang['paragraph_p1'] = "I offer excellent translation services with a primary focus on translating documents 
+from French to English and vice versa. For more information on myself, please see "
+$lang['paragraph_p2'] = "I also offer the following services: "
+$lang['paragraph_p3'] = "and "
+$lang['paragraph_p4'] = "You can see what other people thought of my work "
 /**/
 $lang['nav.home'] = "Home";
 $lang['nav.login'] = "Login";
