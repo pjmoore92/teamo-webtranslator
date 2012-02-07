e1e57f009c0b54a3cb3c39b3d989403632b644e6
alcoheca@gmail.com <alcoheca@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-07 20:18:38 +0000

    added db handling for uploads

---
+++ trunk/application/controllers/service.php
@@ -0,0 +1,132 @@
+<?php if ( ! defined('BASEPATH')) exit('Access denied');
+
+class Service extends CI_Controller {
+
+    public function index()
+    {
+    }
+
+    public function upload()
+    {
+        // Settings
+        $cleanupTargetDir = true; // Remove old files
+        $maxFileAge = 5 * 3600; // Temp file age in seconds
+
+        // 5 minutes execution time
+        @set_time_limit(5 * 60);
+
+        // Uncomment this one to fake upload time
+        // usleep(5000);
+
+        // Get parameters
+        $chunk = $this->input->post('chunk');
+        $chunks = $this->input->post('chunks');
+        $fileName = $this->input->post('name');
+        $job = $this->input->post('job');
+
+        $targetDir = 'incoming';
+        $targetDir = $targetDir.DIRECTORY_SEPARATOR.$job;
+
+        // Clean the fileName for security reasons
+        $fileName = preg_replace('/[^\w\._]+/', '_', $fileName);
+
+        // Make sure the fileName is unique but only if chunking is disabled
+        if ($chunks < 2 && file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName)) {
+            $ext = strrpos($fileName, '.');
+            $fileName_a = substr($fileName, 0, $ext);
+            $fileName_b = substr($fileName, $ext);
+
+            $count = 1;
+            while (file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName_a . '_' . $count . $fileName_b))
+                $count++;
+
+            $fileName = $fileName_a . '_' . $count . $fileName_b;
+        }
+
+        $filePath = $targetDir.DIRECTORY_SEPARATOR.$fileName;
+
+        // Create target dir
+        if (!file_exists($targetDir))
+            @mkdir($targetDir);
+
+        // Remove old temp files	
+        if ($cleanupTargetDir && is_dir($targetDir) && ($dir = opendir($targetDir))) {
+            while (($file = readdir($dir)) !== false) {
+                $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+
+                // Remove temp file if it is older than the max age and is not the current file
+                if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge) && ($tmpfilePath != "{$filePath}.part")) {
+                    @unlink($tmpfilePath);
+                }
+            }
+
+            closedir($dir);
+        } else
+            die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+
+
+        // Look for the content type header
+        if (isset($_SERVER["HTTP_CONTENT_TYPE"]))
+            $contentType = $_SERVER["HTTP_CONTENT_TYPE"];
+
+        if (isset($_SERVER["CONTENT_TYPE"]))
+            $contentType = $_SERVER["CONTENT_TYPE"];
+
+        // Handle non multipart uploads older WebKit versions didn't support multipart in HTML5
+        if (strpos($contentType, "multipart") !== false) {
+            if (isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
+                // Open temp file
+                $out = fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
+                if ($out) {
+                    // Read binary input stream and append it to temp file
+                    $in = fopen($_FILES['file']['tmp_name'], "rb");
+
+                    if ($in) {
+                        while ($buff = fread($in, 4096))
+                            fwrite($out, $buff);
+                    } else
+                        die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+                    fclose($in);
+                    fclose($out);
+                    @unlink($_FILES['file']['tmp_name']);
+                } else
+                    die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+            } else
+                die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+        } else {
+            // Open temp file
+            $out = fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
+            if ($out) {
+                // Read binary input stream and append it to temp file
+                $in = fopen("php://input", "rb");
+
+                if ($in) {
+                    while ($buff = fread($in, 4096))
+                        fwrite($out, $buff);
+                } else
+                    die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+
+                fclose($in);
+                fclose($out);
+            } else
+                die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+        }
+
+        // Check if file has been uploaded
+        if (!$chunks || $chunk == $chunks - 1) {
+            // Strip the temp .part suffix off 
+            rename("{$filePath}.part", $filePath);
+        }
+
+        $this->load->model('translation_model');
+        $this->translation_model->add_orig($job, $filePath);
+
+        // Return JSON-RPC response
+        die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
+
+    }
+
+}
+
+/* End of file service.php */
+/* Location: ./application/controllers/service.php */
