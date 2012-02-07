a0c0f0f9f8ac88d75c08fbc424cd95afff46871b
alcoheca <alcoheca@gmail.com>
2012-02-07 20:18:38 +0000

    added db handling for uploads

---
+++ application/models/translation_model.php
@@ -0,0 +1,44 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Translation_Model extends CI_Model{
+
+    private $_table = 'translation';
+    private $_doc_table = 'document';
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function add_orig($jobid, $filename){
+
+        // check jobid is valid
+        if ($this->db->get_where($this>_table,"jobID", $jobid)->num_rows() > 0) {
+
+            $this->db->trans_start();
+
+            //add file to document table
+            $this->db->insert($this->_doc_table, $filename);
+
+            //query for the ID of the last job added
+            $this->db->select("documentID");
+            $this->db->order_by("documentID", "desc");
+            $this->db->limit(1);
+            $query = $this->db->get($this->_doc_table);
+            
+            if($query->num_rows() == 1)
+                $docID = $query->result();
+
+            // now update job record
+            $this->db->where('jobID', jobid);
+            $this->db->update($this->_table, { 'origDoc', docID });
+
+            //complete transaction
+            $this->db->trans_complete();
+
+            if ($this->db->trans_status() === FALSE){
+                // generate an error... or use the log_message() function to log your error
+            }
+        }
+    }
+}
+
