0c6c657b7868009fc26eddc55e0be2ed9a950f17
pjmoore92 <pjmoore92@gmail.com>
2012-02-08 00:19:53 +0000

    put some stuff in for privacypolicy.php - not final and not checked properly by legal professional

--- application/views/privacypolicy.php
+++ application/views/privacypolicy.php
@@ -1,12 +1,97 @@
 <div class="content">
 <div class="page-header">
-  <h1>Bethel Translations Privacy Policy</h1>
+  <h1>Bethel Translations <small>Privacy Policy</small></h1>
 </div>
 <div class="row">
   <div class="span10">
-    <h2>Main content</h2>
+    <h2>PRIVACY POLICY </h2>
     <p>
-	Our privacy policy will appear here
+We, at <b>Bethel Translations</b> treat the privacy of our visitors with the highest importance. This policy details the measures we take to preserving and safely guarding your privacy when you visit or communicate with our site or personnel.  The Privacy Policy here has been approved and provided by the legal advice resource Legal Centre.
+A detailed explanation of how we may store or otherwise use personal information about you is explained in this Privacy Policy.
+Regular updates of the Privacy Policy are completed, requiring you to check back on this Policy from time to time.
+<br />
+<br />
+[For the purpose of the Data Protection Act of 1998, [ENTER NAME] is our data handler.]
+<br />
+<br />  
+<b>1. Information Collection</b>
+<br />
+      Operation of this site may require collection and processing of the following data:
+<br />
+1.1 Visit details to our site or any resources used on our site are not limited to just location and traffic data, weblogs or other communication information.
+<br />
+1.2 Information given to us when you contact us for any reason.
+<br />
+1.3 Data offered by filled out forms on our site, like a registration or purchase. 
+<br />
+<br />
+<b>2. Cookies</b>
+<br />
+Our advertisers and us may have the occasion to collect information in regards to your computer for our services.  The information is gained in a statistical manner for our use or advertisers on our site. 
+Data gathered will not identify you personally. It is strictly aggregate statistical data about our visitors and how they used our resources on the site.  No identifying personal information will be shared at any time via cookies. 
+Close to the above, data gathering can be about general online use through a cookie file.  When used, cookies are automatically placed in your hard drive where information transferred to your computer can be found.  These cookies are designed to help us correct and improve our site’s services or products for you. 
+You may elect to decline all cookies via your computer.  Every computer has the ability to decline file downloads like cookies.  Your browser has an option to enable the declining of cookies.  If you do decline cookie downloads you may be limited to certain areas of our site, as there are parts of our site that require cookies. 
+Any of our advertisers may also have a use for cookies.  We are not responsible, nor do we have control of the cookies downloaded from advertisements.  They are downloaded only if you click on the advertisement.   
+<br />
+<br />
+<b>3. Your Information and how it is Used</b>
+<br />
+Primarily, we collect and store data about you to help us provide a better service and products to you.  The following are purposes we may use your information for:
+3.1 At anytime you request information from us via a form or other electronic transmission we may use your information to fulfill that request relating to our services and products.  We may also communicate with you on other products or services you may find of interest, only when consent has been provided.
+<br />
+3.2 Contracts we make with you create a commitment, which may require contact or use of your information.
+<br />
+3.3 We have the right to notify you of changes to our website, products or services that could affect our service to you.
+<br />
+3.4 Information on products or services similar to those of an existing consumer purchase may be communicated to you.  The information sent to you in a communication will be similar to the subject of a recent sale.
+<br />
+3.5 We may also use your information or allow a third party use of this data, to offer you information about unrelated products or services you may be interested in.  We or third parties can only communicate if you have consented to such communication and data use.
+<br />
+3.6 New consumers can be contacted by our website or third parties only if consent has been granted, and only for those communications you have granted. 
+<br />
+3.7 An opportunity for declining your consent is provided on our site.  Use this opportunity to withhold your details from us or third parties, regarding data we may collect.
+<br />
+3.8 Be aware we do not reveal identifiable information about you to our advertisers, though we may at times share statistical visitor information with our advertisers.   
+<br />
+<br />
+<b>4. Storage of Personal Data</b>
+<br />
+4.1 The European Economic Area is large, but we may have to transfer data outside of this area.  If data is transferred outside the European Economic Area it will be for storing and processing.  A processing staff operating outside this area may belong to our website or a supplier, in which they can process or store your information.  An example: to process and complete your sale or offer support services we may have to go outside the European Economic Area for the transfer.  When you click submit of your payment details, personal information or other electronic communication you agree to the transfer for storage and processing.  We take all necessary steps for security known to be in agreement with the Privacy Policy found here. 
+<br />
+4.2 Information submitted by you is stored on secure servers we have.  Any payment or transaction details will be encrypted for full safety measures to be in use.
+<br />
+4.3 As you know, transmission of data on the internet is never guaranteed regarding safety.  It is impossible to guarantee your safety with electronic data and transmission.  You are therefore at your own risk if you elect to transmit any data.  When offered you may create a password, but you are responsible for keeping it confidential.  
+<br />
+<br />
+<b>5. Information Sharing</b>
+<br />
+5.1 If necessary, we may share personal information to our group members including such entities as subsidiaries, holding companies and their subsidiaries.  Information is shared only when applicable. 
+<br />            
+5.2 Third party disclosure may be necessary in regards to personal information:
+<br />
+5.2.1 A sale of our business or its assets, in full or part, to a third party may require personal data sharing.
+<br />
+5.2.2 Legally, we may be asked to share and disclose data details.
+<br />
+5.2.3 To assist in reducing credit risk and fraud protection. 
+<br />
+<br />
+<b>6. Third Party Links</b>
+<br />      
+Links on our site that belong to third parties may be found.  These websites have their Privacy Policy, which you agree to when you link to the site.  You   should read this third party policy.  We do not accept claims of liability or responsibility in any way for these policies or links, as we have no way to control the third party sites.  
+<br />
+<br />
+<b>7. Accessing Information</b>
+<br />
+The Data Protection Act 1998 provides you with the right to access the information that we collect about you. The contact information below needs to be used to request access about details we collect and store on you. 
+<br />
+<br />
+<b>8. Contacting Us</b>
+<br />
+We welcome any queries, requests you may have regarding this Privacy Policy, or comments. Please do not hesitate and feel free to contact us using our Contact Page - the link is available in the top menu bar.
+ 
+ 
+
     </p>
   </div>
 </div>
