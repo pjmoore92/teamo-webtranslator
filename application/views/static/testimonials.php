f2e83e281b6b290257dda9433c7f7b9fae93a923
andrei.mustata <andrei.mustata@gmail.com>
2012-02-10 08:13:58 +0000

    moved static pages

---
+++ application/views/static/testimonials.php
@@ -0,0 +1,62 @@
+<?php if ( ! defined('BASEPATH')) exit('Access denied'); ?>
+
+<div class="content">
+<div class="page-header">
+  <h1>Bethel Translations <small>Testimonials page</small></h1>
+</div>
+<div class="row">
+  <div class="span10">
+    <h2>Main content</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
+        tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis 
+        nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
+        Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel 
+        illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui 
+        blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber 
+        tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat 
+        facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit 
+        eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt 
+        saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.
+    </p>
+
+  </div>
+  <div class="span4">
+        <h2>Sidebar</h2>
+        <!--<p>
+                Some more lorem ipsum on the side here Typi non habent claritatem insitam; est usus legentis in
+                 iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod
+                  ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem
+        </p>-->
+        <ul class="media-grid">
+                  <li>
+                    <a href="#">
+                      <img class="thumbnail" src="http://placehold.it/210x150" alt="">
+                    </a>
+                  </li>
+                </ul>
+  </div>
+</div>
+</div>
+
+  <hr>
+  
+<div class="row">
+  <div class="span4">
+    <h2>Testimonial</h2>
+    <p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+    <p><a class="btn" href="#">View details &raquo;</a></p>
+  </div>
+  <div class="span4">
+    <h2>Testimonial</h2>
+     <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+    <p><a class="btn" href="#">View details &raquo;</a></p>
+ </div>
+  <div class="span4">
+    <h2>Testimonial</h2>
+    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+    <p><a class="btn" href="#">View details &raquo;</a></p>
+  </div>
+</div>
+
+
