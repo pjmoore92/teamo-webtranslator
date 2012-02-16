0fe99cb7c4beab6e55a58115ab2edc533f50745f
alcoheca <alcoheca@gmail.com>
2012-02-16 14:40:52 +0000

    fixed missing images

--- application/views/template.php
+++ application/views/template.php
@@ -23,10 +23,10 @@
     </style>
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="/images/favicon.ico">
+<!--    <link rel="shortcut icon" href="/images/favicon.ico"> 
     <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/images/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-touch-icon-114x114.png"> -->
    
     <script type="text/javascript" src="<?php echo base_url('/js/jquery/jquery-1.7.1.min.js'); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('/js/jquery/jquery.form.js'); ?>"></script>
