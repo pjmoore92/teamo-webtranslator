57b9b8f77029bbc2873e8c975747e83d898707cc
pjmoore92@gmail.com <pjmoore92@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-20 09:30:47 +0000

    small change -  changed stats and customer model to provide more statistics for joelle

--- trunk/application/models/customer_model.php
+++ trunk/application/models/customer_model.php
@@ -29,12 +29,44 @@ class Customer_model extends CI_Model{
         if ($query->num_rows() > 0) return $query->result();
         return NULL;
     }
+    
+    
+    public function countAllCustomers(){
+	$users = $this->db->count_all('users');	
+	return $users;
+    }
 	
-	public function countActiveCustomers(){
-		$query = $this->db->query("SELECT COUNT(activated) FROM `{$this->_table}` WHERE activated = '1'");
-		
-		return $query->result();
-	}
+	
+    public function countActiveCustomers(){
+	$query = $this->db->query("SELECT COUNT(activated) FROM `users` WHERE activated = '1'");
+	return $query->result();
+    }
+    
+    public function countPending(){
+	$query = $this->db->query("SELECT COUNT(status) FROM `job` WHERE status = 'QuoteReq'");
+	return $query->result();
+    }
+    
+    public function countAwaitingPayment(){
+	$query = $this->db->query("SELECT COUNT(status) FROM `job` WHERE status = 'QuoteSent'");
+	return $query->result();
+    }
+    
+    public function countPaid(){
+	$query = $this->db->query("SELECT COUNT(status) FROM `job` WHERE status = 'Paid'");
+	return $query->result();
+    }
+    
+    public function countTranslated(){
+	$query = $this->db->query("SELECT COUNT(status) FROM `job` WHERE status = 'Translated'");
+	return $query->result();
+    }
+    
+    public function countTotal(){
+	$query = $this->db->query("SELECT COUNT(status) FROM `job`");
+	return $query->result();
+    }
+
 
 }
 
