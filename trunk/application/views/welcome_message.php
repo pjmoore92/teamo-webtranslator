2b266d3cc4b12758f25b0bfc272595625578f660
s.hayton91@gmail.com <s.hayton91@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-20 14:57:18 +0000

    Fixed flags on home page

--- trunk/application/views/welcome_message.php
+++ trunk/application/views/welcome_message.php
@@ -2,11 +2,12 @@
 
 <!-- TODO horizontal sliding Ads a la BBC? -->
 <div class="hero-unit">
-<h1><?php echo lang('helloWorld') ?></h1>
+<h1><?php echo lang('helloWorld') ?>
 
-<a href="/en"><img src="/images/uk.jpg"></a>
-<a href="/fr"><img src="/images/france.jpg"></a>
-<a href="/it"><img src="/images/italy.jpg"></a>
+<a href="/en"><img src="/images/uk.png"></a>
+<a href="/fr"><img src="/images/france.png"></a>
+<a href="/it"><img src="/images/italy.png"></a>
+</h1>
 <p>Vestibulum id ligula porta felis euismod semper. Integer posuere erat
 a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non
 commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
