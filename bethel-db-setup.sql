aa5d5eb06b3e5bb55e95a734ee26cac5dcc41287
alcoheca <alcoheca@gmail.com>
2012-02-21 17:04:05 +0000

    update initial sql file

--- bethel-db-setup.sql
+++ bethel-db-setup.sql
@@ -1,91 +1,81 @@
--- --------------------------------------------------------
--- USE TO CREATE A FRESH SET OF TABLES 
+-- phpMyAdmin SQL Dump
+-- version 3.4.9
+-- http://www.phpmyadmin.net
 --
+-- Host: localhost
+-- Generation Time: Feb 21, 2012 at 05:02 PM
+-- Server version: 5.0.92
+-- PHP Version: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 --
 -- Database: `claddach_igni348`
 --
 
 -- --------------------------------------------------------
---
--- Table structure for table `customer`
---
-DROP TABLE IF EXISTS `customer`;
-DROP TABLE IF EXISTS `document`;
-DROP TABLE IF EXISTS `job`;
-DROP TABLE IF EXISTS `translations`;
-DROP TABLE IF EXISTS `ci_sessions`;
-DROP TABLE IF EXISTS `login_attempts`;
-DROP TABLE IF EXISTS `ci_sessions`;
-DROP TABLE IF EXISTS `user_autologin`;
-DROP TABLE IF EXISTS `user_profiles`;
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE IF NOT EXISTS `customer` (
-  `customerID` int(11) NOT NULL AUTO_INCREMENT,
-  `fullName` text NOT NULL,
-  PRIMARY KEY  (`customerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
---
--- Table structure for table `document`
---
-
-CREATE TABLE IF NOT EXISTS `document` (
-  `documentID` int(11) NOT NULL AUTO_INCREMENT,
-  `filePath` varchar(256) NOT NULL,
-  PRIMARY KEY  (`documentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
---
--- Table structure for table `jobs`
---
-
-CREATE TABLE IF NOT EXISTS `job` (
-  `jobID` int(11) NOT NULL auto_increment,
-  `customerID` int(11) NOT NULL,
-  `status` enum('QuoteReq','QuoteSent','QuoteAccept','QuoteDeclined','Paid','Translated','Complete') NOT NULL,
-  `quote` int(10) default NULL,
-  `dateRequested` date NOT NULL,
-  `dateDue` date NOT NULL,
-  `fromLanguage` text NOT NULL,
-  `toLanguage` text NOT NULL,
-   PRIMARY KEY  (`jobID`),
-   FOREIGN KEY (`customerID`) REFERENCES users (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
---
--- Table structure for table `translation`
---
-
-CREATE TABLE IF NOT EXISTS `translation` (
-  `translationID` int(11) NOT NULL auto_increment,
-  `jobID` int(11) NOT NULL,
-  `origDoc` int(11) NOT NULL,
-  `translatedDoc` int(11) default NULL,
-  PRIMARY KEY  (`translationID`),
-  FOREIGN KEY `origDoc` REFERENCES `document` (`documentID`),
-  FOREIGN KEY `translatedDoc` REFERENCES `document` (`documentID`),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
--- ------------- TankAuth tables -------------
 
 --
 -- Table structure for table `ci_sessions`
 --
 
+DROP TABLE IF EXISTS `ci_sessions`;
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
-  `session_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '0',
-  `ip_address` varchar(16) COLLATE utf8_bin NOT NULL DEFAULT '0',
-  `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`session_id`)
+  `session_id` varchar(40) collate utf8_bin NOT NULL default '0',
+  `ip_address` varchar(16) collate utf8_bin NOT NULL default '0',
+  `user_agent` varchar(150) collate utf8_bin NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL default '0',
+  `user_data` text collate utf8_bin NOT NULL,
+  PRIMARY KEY  (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` (
+  `customerID` int(11) NOT NULL,
+  `fullName` text NOT NULL,
+  PRIMARY KEY  (`customerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `document`
+--
+
+DROP TABLE IF EXISTS `document`;
+CREATE TABLE IF NOT EXISTS `document` (
+  `documentID` int(11) NOT NULL auto_increment,
+  `filePath` varchar(256) NOT NULL,
+  PRIMARY KEY  (`documentID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job`
+--
+
+DROP TABLE IF EXISTS `job`;
+CREATE TABLE IF NOT EXISTS `job` (
+  `jobID` int(11) NOT NULL auto_increment,
+  `customerID` int(11) NOT NULL,
+  `status` enum('QuoteReq','QuoteSent','QuoteAccept','QuoteDeclined','Paid','Translated','Complete') collate utf8_bin NOT NULL,
+  `quote` int(10) default NULL,
+  `dateRequested` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `dateDue` date NOT NULL,
+  `fromLanguage` enum('english','italian') collate utf8_bin NOT NULL,
+  `toLanguage` enum('french') collate utf8_bin NOT NULL,
+  `currency` enum('gbp','eur') collate utf8_bin NOT NULL,
+  PRIMARY KEY  (`jobID`),
+  KEY `customerID` (`customerID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=934247 ;
 
 -- --------------------------------------------------------
 
@@ -93,42 +83,46 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 -- Table structure for table `login_attempts`
 --
 
+DROP TABLE IF EXISTS `login_attempts`;
 CREATE TABLE IF NOT EXISTS `login_attempts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(40) COLLATE utf8_bin NOT NULL,
-  `login` varchar(50) COLLATE utf8_bin NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `id` int(11) NOT NULL auto_increment,
+  `ip_address` varchar(40) collate utf8_bin NOT NULL,
+  `login` varchar(50) collate utf8_bin NOT NULL,
+  `time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_autologin`
+-- Table structure for table `roles`
 --
 
-CREATE TABLE IF NOT EXISTS `user_autologin` (
-  `key_id` char(32) COLLATE utf8_bin NOT NULL,
-  `user_id` int(11) NOT NULL DEFAULT '0',
-  `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
-  `last_ip` varchar(40) COLLATE utf8_bin NOT NULL,
-  `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`key_id`,`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` tinyint(1) NOT NULL auto_increment,
+  `role` varchar(255) NOT NULL,
+  `default` tinyint(2) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_profiles`
+-- Table structure for table `translation`
 --
 
-CREATE TABLE IF NOT EXISTS `user_profiles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `country` varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  `website` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+DROP TABLE IF EXISTS `translation`;
+CREATE TABLE IF NOT EXISTS `translation` (
+  `translationID` int(11) NOT NULL auto_increment,
+  `jobID` int(11) NOT NULL,
+  `name` varchar(256) collate utf8_bin NOT NULL,
+  `origDoc` int(11) NOT NULL,
+  `translatedDoc` int(11) default NULL,
+  PRIMARY KEY  (`translationID`),
+  KEY `origDoc` (`origDoc`),
+  KEY `translatedDoc` (`translatedDoc`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -136,21 +130,57 @@ CREATE TABLE IF NOT EXISTS `user_profiles` (
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE utf8_bin NOT NULL,
-  `password` varchar(255) COLLATE utf8_bin NOT NULL,
-  `email` varchar(100) COLLATE utf8_bin NOT NULL,
-  `activated` tinyint(1) NOT NULL DEFAULT '1',
-  `banned` tinyint(1) NOT NULL DEFAULT '0',
-  `ban_reason` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `new_password_key` varchar(50) COLLATE utf8_bin DEFAULT NULL,
-  `new_password_requested` datetime DEFAULT NULL,
-  `new_email` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `new_email_key` varchar(50) COLLATE utf8_bin DEFAULT NULL,
-  `last_ip` varchar(40) COLLATE utf8_bin NOT NULL,
-  `last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(50) collate utf8_bin NOT NULL,
+  `password` varchar(255) collate utf8_bin NOT NULL,
+  `email` varchar(100) collate utf8_bin NOT NULL,
+  `activated` tinyint(1) NOT NULL default '1',
+  `banned` tinyint(1) NOT NULL default '0',
+  `ban_reason` varchar(255) collate utf8_bin default NULL,
+  `new_password_key` varchar(50) collate utf8_bin default NULL,
+  `new_password_requested` datetime default NULL,
+  `new_email` varchar(100) collate utf8_bin default NULL,
+  `new_email_key` varchar(50) collate utf8_bin default NULL,
+  `last_ip` varchar(40) collate utf8_bin NOT NULL,
+  `last_login` datetime NOT NULL default '0000-00-00 00:00:00',
+  `created` datetime NOT NULL default '0000-00-00 00:00:00',
+  `modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `role_id` tinyint(1) NOT NULL default '1',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=182 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_autologin`
+--
+
+DROP TABLE IF EXISTS `user_autologin`;
+CREATE TABLE IF NOT EXISTS `user_autologin` (
+  `key_id` char(32) collate utf8_bin NOT NULL,
+  `user_id` int(11) NOT NULL default '0',
+  `user_agent` varchar(150) collate utf8_bin NOT NULL,
+  `last_ip` varchar(40) collate utf8_bin NOT NULL,
+  `last_login` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`key_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `job`
+--
+ALTER TABLE `job`
+  ADD CONSTRAINT `job_ibfk_2` FOREIGN KEY (`customerID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `translation`
+--
+ALTER TABLE `translation`
+  ADD CONSTRAINT `translation_ibfk_3` FOREIGN KEY (`origDoc`) REFERENCES `document` (`documentID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `translation_ibfk_4` FOREIGN KEY (`translatedDoc`) REFERENCES `document` (`documentID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
