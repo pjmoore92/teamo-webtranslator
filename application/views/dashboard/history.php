6424d3b4c6506269f7c6038db3ea659e30dd125e
andrei.mustata <andrei.mustata@gmail.com>
2012-02-23 06:45:04 +0000

    dashboard: created a template for all the job-related pages, and got rid of those particular view files

--- application/views/dashboard/history.php
+++
@@ -1,4 +0,0 @@
-        <div class="page-header">
-          <h1><?php echo ucfirst($role); ?> Dashboard <small>Historical Jobs</small></h1>
-          <h2>Hello, <?php echo $client_name; ?>!</h2>
-        </div>
--- application/views/dashboard/template.php
+++ application/views/dashboard/template.php
@@ -41,6 +41,24 @@
 <div class="modal-footer"></div>
 </div>
 
-<?php $this->load->view('dashboard/'.$type); ?>
+<div class="page-header">
+  <h1><?php echo ucfirst($role); ?> Dashboard <small><?php echo ucfirst($subtitle); ?> Jobs</small></h1>
+  <h2>Hello, <?php echo $client_name; ?>!</h2>
+</div>
+<div class="row">
+  <div class="span9">
+    <?php
+        if($jobs_list == NULL):
+        echo "You have no {$subtitle} jobs.";
+        else:
+    ?>
+    <div class="accordion" id="accordion2">
+    <?php
+      foreach($jobs_list as $job)
+        $this->load->view('dashboard/job', array('job' => $job, 'subtitle' => $subtitle));
+    ?>
+    </div>
 
+      <?php endif; ?>
+  </div>
 </div>
