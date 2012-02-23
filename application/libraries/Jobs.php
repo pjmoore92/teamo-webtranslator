39b98c3088aa96c4ef6dceb593da5417064fab90
alcoheca <alcoheca@gmail.com>
2012-02-23 09:19:54 +0000

    remove duplicate methods

--- application/libraries/Jobs.php
+++ application/libraries/Jobs.php
@@ -8,13 +8,11 @@ class Jobs{
 
 	}
 
-
 	public function add_job($args){
 		
 		$job_data = array(
 			'customerID' => $args['customerID'],
 			'status' => 'QuoteReq',
-			// 'dateDue' => date('Y-m-d H:i:s', time() + (7 * 24 * 60 * 60)),
 			'dateDue' => $args['deadline'],
 			'toLanguage' => $args['toLanguage'],
 			'fromLanguage' => $args['fromLanguage']
@@ -47,37 +45,7 @@ class Jobs{
 	}
 
 	private function _upload_files(){
-
-		if(count($_FILES["docs"]) == 0)
-			return FALSE;
-
-		$config['upload_path'] = './testingupload/';
-		$config['allowed_types'] = 'pdf|docx|doc';
-
-		$this->ci->load->library('upload', $config);
-
-		foreach ($_FILES["docs"]["error"] as $key => $error) {
-
-
-			if ( ! $this->ci->upload->do_upload("docs") ){
-				$data['error'] = array('error' => $this->ci->upload->display_errors());
-				// $this->load->view('testing/upload_form', $error);
-
-			}
-			else{
-				$data = array('upload_data' => $this->ci->upload->data());
-				// $this->load->view('testing/upload_success', $data);
-			}
-
-			//  sample code from php.com
-			// if ($error == UPLOAD_ERR_OK) {
-			// 	$tmp_name = $_FILES["pictures"]["tmp_name"][$key];
-			// 	$name = $_FILES["pictures"]["name"][$key];
-			// 	move_uploaded_file($tmp_name, "data/$name");
-			// }
-		}
-
-		return $data;
+		return NULL;
 	}
 
 }
--- application/models/job_model.php
+++ application/models/job_model.php
@@ -11,48 +11,6 @@ class Job_model extends CI_Model{
         parent::__construct();
     }
 
-    public function get_by_status($status, $customerID =  NULL) {
-        $ci =& get_instance();
-        $ci->load->model('translation');
-
-        $this->db->where('status', $status);
-        if ($customerID != NULL) $this->db->where("{$this->_table}.customerID", $customerID);
-        
-        //JOIN with user_profiles table to get the user specific data
-        $this->db->join($this->_cust_table, "{$this->_cust_table}.customerID = {$this->_table}.customerID");
-        $this->db->join('users', "users.id = {$this->_table}.customerID");
-
-        $this->db->select('jobID, status, dateRequested, dateDue, fromLanguage, toLanguage, currency, quote, fullName, email');
-        $query = $this->db->get($this->_table);
-        if($query->num_rows() > 0) {
-            $jobs = array();
-            foreach ($query->result() as $job) {
-                $translations = $ci->translation->get($job->jobID);
-                $jobs[] = array($job, $translations);
-            }
-            return $jobs;
-        }
-        else
-            return NULL;
-    }
-
-    /*
-      needs to check the date
-     */
-    public function get_all_history($customerID = NULL){
-        // $cur_date = getdate();
-        // $due_date = $this->db->get('dateDue');
-        // $interval = $cur_date->diff($due_date);
-
-        $this->db->where('status', 'Translated');        
-        if ($customerID != 0)
-            $this->db->where('customerID', $customerID);
-        $query = $this->db->get($this->_table);
-        /* Assumes completed jobs are held in translations for 90 days, then moved to history after that period.*/
-        if($query->num_rows() > 0 /*&& $interval->d >90*/ ) return $query->result();
-        return NULL;
-    }
-
     public function add_job($job_data){
 
         //Start transaction, lol
@@ -78,6 +36,18 @@ class Job_model extends CI_Model{
         }
         
         return $jobID;
+    }
+
+    public function get_jobs($status, $customerID = NULL){
+
+        if($customerID != NULL) $this->db->where("{$this->_table}.customerID", $customerID);
+        $this->db->where('status', $status);
+        
+        $this->db->join($this->_cust_table, "{$this->_cust_table}.customerID = {$this->_table}.customerID");
+        
+        $query = $this->db->get($this->_table);
+        if($query->num_rows() > 0) return $query->result();
+        return NULL;
     }
 
     public function update_job($job_data){
@@ -109,17 +79,6 @@ class Job_model extends CI_Model{
         $this->db->update($this->_table, array('status'=> $status));
     }
 
-    public function get_jobs($status, $customerID = NULL){
-
-        if($customerID != NULL) $this->db->where("{$this->_table}.customerID", $customerID);
-        $this->db->where('status', $status);
-        
-        $this->db->join($this->_cust_table, "{$this->_cust_table}.customerID = {$this->_table}.customerID");
-        
-        $query = $this->db->get($this->_table);
-        if($query->num_rows() > 0) return $query->result();
-        return NULL;
-    }
 }
 
 /* End of file job_model.php */
