526871a8a4460294ca0356d609f62c7667d35a6b
andrei.mustata <andrei.mustata@gmail.com>
2012-02-25 03:10:45 +0000

    Dashboard: moved inline JS to external files; \nClient: updated add a job page; added file upload

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -34,34 +34,78 @@ class Dashboard extends MY_Controller {
         public function add_job(){
 
                 if(!$this->input->is_ajax_request()){
-                        return json_encode(array('error' => 'Somthin\' ain\' right :<'));
+                        redirect('');
                 }
                 else{
 
+                        $this->output->set_content_type('application/json');
+
+
+                        $config = array(
+                                'new_job' => array(
+                                        array(
+                                                'field' => 'lang_from',
+                                                'label' => 'Source language',
+                                                'rules' => 'required|xss_clean|callback_check_lang_from'
+                                        ),
+
+                                        array(
+                                                'field' => 'lang_to',
+                                                'label' => 'Translation language',
+                                                'rules' => 'required|xss_clean|callback_check_lang_to'
+                                        ),
+                                        
+                                        array(
+                                                'field' => 'deadline',
+                                                'label' => 'Deadline',
+                                                'rules' => 'required|xss_clean|callback_check_deadline'
+                                        ),
+                                        
+                                        array(
+                                                'field' => 'currency',
+                                                'label' => 'Currency',
+                                                'rules' => 'required|xss_clean|callback_check_currency'
+                                        )
+                                )
+                        );
+                        
                         $this->load->library('form_validation');
 
-                        $this->form_validation->set_rules('toLanguage', 'To language', 'required');
-                        $this->form_validation->set_rules('fromLanguage', 'From language', 'required');
+                        /* run validation for the registration form */
+                        $this->form_validation->set_rules($config['new_job']);
+
+                        /* Form validation error messages */
+                        $this->form_validation->set_message('required', 'Field <strong>%s</strong> is required');
+                        $this->form_validation->set_message('valid_email', '<strong>%s</strong> is not a valid e-mail');
 
                         if ($this->form_validation->run() == FALSE){
-                                return json_encode(array('error' => 'Somthin\' ain\' right :<'));
+                                $this->output->set_output(
+                                        json_encode(array('error'=> sprintf("%s", validation_errors())))
+                                );
                         }
                         else{
 
-                                $dateDue = date('Y-m-d H:i:s', time() + (7 * 24 * 60 * 60));
+                                $job = new stdClass;
+                                $job->customer_id = $this->_user_id;
+                                $job->deadline = $this->form_validation->set_value('deadline');
+                                $job->lang_to = $this->form_validation->set_value('lang_to');
+                                $job->lang_from = $this->form_validation->set_value('lang_from');
 
-                                $job_data = array(
-                                        'customerID' => $this->_user_id,
+
+                                $this->load->library('jobs');
+
+                                $details = array(
+                                        'customerID' => $job->customer_id,
                                         'status' => 'QuoteReq',
-                                        'dateDue' => $dateDue,
-                                        'toLanguage' => $this->form_validation->set_value('toLanguage'),
-                                        'fromLanguage' => $this->form_validation->set_value('fromLanguage')
+                                        'fromLanguage' => $job->lang_from,
+                                        'toLanguage' => $job->lang_to,
+                                        'deadline' => $job->deadline
                                 );
+                                
+                                /* add the job to the DB */
+                                $job->id = $this->jobs->add_job($details);
 
-                                $this->load->model('job_model');
-                                $this->job_model->add_job($job_data);
-
-                                die(json_encode(array('response' => 'WOOHOO!')));
+                                $this->output->set_output((json_encode($job)));
                         }
                 }
         }
@@ -151,4 +195,76 @@ class Dashboard extends MY_Controller {
                 $this->template->set('title', $this->_data['role']. ' Dashboard -');
                 $this->template->load('template', $this->_view_template, $this->_data);
         }
+
+
+    /**
+     * Check if 'From' language is allowed
+    */
+    public function check_lang_from($lang){
+        $allowed = array('english', 'italian');
+        
+        if( !in_array($lang, $allowed)){
+                $this->form_validation->set_message('check_lang_from', 'That language is not allowed');
+                return FALSE;
+        }
+        else
+                return TRUE;
+    }
+    
+    /**
+     * Check if 'From' language is allowed
+    */
+    public function check_lang_to($lang){
+        $allowed = array('french');
+        
+        // if( !in_array($lang, $allowed)){
+        if($lang != 'french'){
+                $this->form_validation->set_message('check_lang_to', 'That language is not allowed');
+                return FALSE;
+        }
+        else
+                return TRUE;
+    }
+
+        /**
+         * Check if date is valid (YYY-MM-DD)
+         */
+    public function check_deadline($date){
+        if(strlen($date) == 10){
+                $year = substr($date, 0, 4);
+                $month = substr($date, 5, 2);
+                $day = substr($date, 8, 2);
+
+                if(
+                        ($year == FALSE || $year == '') &&
+                        ($month == FALSE || $month == '') &&
+                        ($day == FALSE || $day == '')
+                ){
+                        
+                        $this->form_validation->set_message('check_deadline', 'Date not valid');
+                        return FALSE;
+                }
+
+                if(is_numeric($year) && is_numeric($month) && is_numeric($day))
+                        if(checkdate((int)$month, (int)$day, (int)$year))
+                                return TRUE;
+        }
+        
+        $this->form_validation->set_message('check_deadline', 'Date not valid');
+        return FALSE;
+    }
+
+    /**
+     *
+     */
+    public function check_currency($curr){
+        $allowed = array('gbp', 'eur');
+        
+        if( !in_array($curr, $allowed)){
+                $this->form_validation->set_message('check_currency', 'That currency is not allowed');
+                return FALSE;
+        }
+        else
+                return TRUE;
+    }
 }
