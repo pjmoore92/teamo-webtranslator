b6df133a841be14169f0005fd1c3bbcc5f331b69
andrei.mustata@gmail.com <andrei.mustata@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-25 00:42:37 +0000

    Main page: moved JS scripts in separate files

---
+++ trunk/js/upload.js
@@ -0,0 +1,56 @@
+$(function() {
+
+  var uploader = new plupload.Uploader({
+        runtimes : 'html5,flash',
+    browse_button : 'pickfiles',
+    container : 'container',
+        unique_names : 'true',
+    max_file_size : '15mb',
+    url : '/en/service/upload/',
+    flash_swf_url : '/js/plupload/plupload.flash.swf',
+    silverlight_xap_url : '/js/plupload/plupload.silverlight.xap',
+    filters : [
+      {title : "Documents", extensions : "pdf,doc,docx,rtf,txt"}
+            ],
+        multipart_params : { job : -2 }
+  });
+
+  $('#uploadfiles').click(function(e) {
+    uploader.start();
+    e.preventDefault();
+  });
+
+  uploader.init();
+
+  uploader.bind('FilesAdded', function(up, files) {
+    $.each(files, function(i, file) {
+      $('#filelist').append(
+        '<div id="' + file.id + '">' +
+        file.name + ' (' + plupload.formatSize(file.size) + ') <b></b>' +
+      '</div>');
+    });
+
+    up.refresh(); // Reposition Flash/Silverlight
+  });
+
+  uploader.bind('UploadProgress', function(up, file) {
+    $('#' + file.id + " b").html(file.percent + "%");
+  });
+
+  uploader.bind('BeforeUpload', function(up, file) {
+    up.settings.multipart_params = { job : jobID };
+  });
+
+  uploader.bind('Error', function(up, err) {
+    $('#filelist').append("<div>" + err.message +
+      (err.file ? err.file.name : "") +
+      "</div>"
+    );
+
+    up.refresh(); // Reposition Flash/Silverlight
+  });
+
+  uploader.bind('FileUploaded', function(up, file) {
+    $('#' + file.id + " b").html("100%");
+  });
+});
