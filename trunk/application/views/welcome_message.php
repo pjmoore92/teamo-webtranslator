f800f2a6f45161f1b3b8a0c021a12cf706263579
pjmoore92@gmail.com <pjmoore92@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-27 21:22:00 +0000

    made submit button larger after evaluation feedback.

--- trunk/application/views/welcome_message.php
+++ trunk/application/views/welcome_message.php
@@ -91,7 +91,7 @@ translation in your chosen language.</p>
 <br>
   <div class="span3">&nbsp;</div>
   <div class="span3 offset1" style="text-align:center">
-    <p><a id="register-submit" class="btn btn-success" href="#" data-controls-modal="modal-from-dom-register-message" 
+    <p><a id="register-submit" class="btn btn-success btn-large" href="#" data-controls-modal="modal-from-dom-register-message" 
               data-backdrop="true" data-keyboard="true">Submit + Register! &raquo;</a></p>
   <!--  <input type="submit" class="btn btn-success" value="Get your quote!"> -->
   </div>
