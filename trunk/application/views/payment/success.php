2a3a9edf9e21a64302fd1abb052840e314590c0f
andrei.mustata@gmail.com <andrei.mustata@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-02-27 17:10:00 +0000

    Payment: put the success and failure messages in the dashboard template

--- trunk/application/views/payment/success.php
+++ trunk/application/views/payment/success.php
@@ -1,3 +1,3 @@
-	<h1>Payment Gateway Tests - Paypal Success</h1>
-	<p>Thank you!</p>
-	<p>We have received your order and have started processing it. We will let you know as soon as it is being confirmed by Paypal.</p>
+<h3>The PayPal transaction was succesful!</h3>
+<p>Thank you!</p>
+<p>We have received your order and have started processing it. We will let you know as soon as it is being confirmed by Paypal.</p>
