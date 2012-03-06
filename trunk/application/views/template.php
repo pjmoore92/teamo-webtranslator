95a1cca0b1cba3b68adac9f8934264b1b0404b44
pjmoore92@gmail.com <pjmoore92@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-03-06 09:55:09 +0000

    removed unused form.js

--- trunk/application/views/template.php
+++ trunk/application/views/template.php
@@ -27,7 +27,6 @@
     <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-touch-icon-114x114.png"> -->
    
     <script type="text/javascript" src="<?php echo base_url('/js/jquery/jquery-1.7.1.min.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('/js/jquery/jquery.form.js'); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('/js/bootstrap-modal.js'); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('/js/bootstrap-dropdown.js'); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('/js/bootstrap-alert.js'); ?>"></script>
