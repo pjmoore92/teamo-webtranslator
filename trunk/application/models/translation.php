3a9d0820a409c274d2fa93c58ecdb93c16667385
alcoheca@gmail.com <alcoheca@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-03-07 09:22:07 +0000

    add: translation_model->add_trans

--- trunk/application/models/translation.php
+++ trunk/application/models/translation.php
@@ -38,6 +38,61 @@ class Translation extends CI_Model{
         return NULL;
     }
 
+    public function add_trans($trans, $docname, $filename){
+        //FIXME Don't repeat yourself
+
+        // check translation is valid
+        $this->db->where("translationID", $trans);
+        $query = $this->db->get($this->_table);
+        if ($query->num_rows() > 0) {	
+
+            $this->db->trans_start();
+
+            //FIXME 
+            //truncate filename from /home/claddach/public_html/alasdaircampbell.com/*
+            $filename = substr($filename, 48);
+
+            //add file to document table
+            $record = array(
+                'filePath' => $filename);
+            $this->db->insert($this->_doc_table, $record);
+
+            //query for the ID of the last document added
+            $this->db->select("documentID");
+            $this->db->order_by("documentID", "desc");
+            $this->db->limit(1);
+            $query = $this->db->get($this->_doc_table);
+
+            if($query->num_rows() != 1)
+            {
+                //log db error
+            }
+
+            $row = $query->row();
+            $docid = $row->documentID;
+
+            // now update the translation job record
+            $record = array(
+                'translatedDoc' => $docid
+            );
+            $this->db->where("translationID", $trans);
+            $this->db->update($this->_table, $record);
+
+            //complete transaction
+            $this->db->trans_complete();
+
+            if ($this->db->trans_status() === FALSE){
+                log_message('error', 'transaction failed in translation::add_orig');
+                return false;
+            }
+            return true;
+        }
+        else {
+            log_message('error', 'jobid '.$jobid.' did not match a record');
+            return false;
+        }
+    }
+
     public function add_orig($jobid, $docname, $filename){
 
         // check jobid is valid
