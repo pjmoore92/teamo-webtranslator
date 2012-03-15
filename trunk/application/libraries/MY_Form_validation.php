3d643b7a49504aeb3813ba3529aaa84b20fec414
andrei.mustata@gmail.com <andrei.mustata@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-03-15 21:48:13 +0000

    form_validation: moved custom class in application/libraries - works now

--- trunk/application/libraries/MY_Form_validation.php
+++ trunk/application/libraries/MY_Form_validation.php
@@ -1,20 +1,36 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class MY_Form_validation extends Form_validation {
+class MY_Form_validation extends CI_Form_validation {
+
+    protected $CI;
+
+    
+    function __construct(){
+        parent::__construct();
+
+        $this->CI =& get_instance();
+    }
 
     
     /**
      * Check currency
+     * 
+     * Checks if the supplied currency is accepted.
+     * 
      * @access 	public
      * @param 	string
      * @return 	bool
      */
     public function check_currency($str){
-    	$this->CI->load('business');
 
+        // load the 'business' config file
+        $this->CI->load->config('business');
+
+        // load the accepted currencies array
     	$allowed = $this->CI->config->item('currencies');
     	
     	if( !in_array($str, $allowed)){
+           //FIXME move this in the language file
     		$this->CI->form_validation->set_message('check_currency', 'That currency is not allowed');
     		return FALSE;
     	}
