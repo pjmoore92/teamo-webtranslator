e2b18ee79ebef42e90f7cac91da65b1dcbb37194
andrei.mustata <andrei.mustata@gmail.com>
2012-03-17 17:11:01 +0000

    Registration: changed activation screen to load the login form and prepend the success/error message on top of it

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -6,13 +6,14 @@ $login = array(
 	'maxlength'	=> 80,
 	'size'	=> 30,
 );
-if ($login_by_username AND $login_by_email) {
-	$login_label = 'Email or login';
-} else if ($login_by_username) {
-	$login_label = 'Login';
-} else {
-	$login_label = 'Email';
-}
+if( isset($login_by_username) OR isset($login_by_email) )
+	if ($login_by_username AND $login_by_email) {
+		$login_label = 'Email or login';
+	} else if ($login_by_username) {
+		$login_label = 'Login';
+	} else {
+		$login_label = 'Email';
+	}
 $password = array(
 	'name'	=> 'password',
 	'id'	=> 'password',
@@ -25,20 +26,19 @@ $remember = array(
 	'checked'	=> set_value('remember'),
 	'style' => 'margin:0;padding:0',
 );
-$captcha = array(
-	'name'	=> 'captcha',
-	'id'	=> 'captcha',
-	'maxlength'	=> 8,
-);
 ?>
 
-<?php
-	if( $message != NULL )
-		echo $message;
-?>
+<?php if( $message != NULL ): ?>
+	<?php
+		$message->type = ($message->type != NULL) ? 'alert-'.$message->type : '';
+	?>
+    <div class="alert <?php echo $message->type; ?>">
+    	<a class="close" data-dismiss="alert">×</a>
+    	<?php echo $message->text; ?>
+    </div>
+<?php endif; ?>
 
-
-<?php echo form_open($this->uri->uri_string()); ?>
+<?php echo form_open('/en/auth/login'); ?>
 <table>
 	<tr>
 		<td><?php echo form_label($login_label, $login['id']); ?></td>
@@ -50,43 +50,6 @@ $captcha = array(
 		<td><?php echo form_password($password); ?></td>
 		<td style="color: red;"><?php echo form_error($password['name']); ?><?php echo isset($errors[$password['name']])?$errors[$password['name']]:''; ?></td>
 	</tr>
-
-	<?php if ($show_captcha) {
-		if ($use_recaptcha) { ?>
-	<tr>
-		<td colspan="2">
-			<div id="recaptcha_image"></div>
-		</td>
-		<td>
-			<a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a>
-			<div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type('audio')">Get an audio CAPTCHA</a></div>
-			<div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a></div>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<div class="recaptcha_only_if_image">Enter the words above</div>
-			<div class="recaptcha_only_if_audio">Enter the numbers you hear</div>
-		</td>
-		<td><input type="text" id="recaptcha_response_field" name="recaptcha_response_field" /></td>
-		<td style="color: red;"><?php echo form_error('recaptcha_response_field'); ?></td>
-		<?php echo $recaptcha_html; ?>
-	</tr>
-	<?php } else { ?>
-	<tr>
-		<td colspan="3">
-			<p>Enter the code exactly as it appears:</p>
-			<?php echo $captcha_html; ?>
-		</td>
-	</tr>
-	<tr>
-		<td><?php echo form_label('Confirmation Code', $captcha['id']); ?></td>
-		<td><?php echo form_input($captcha); ?></td>
-		<td style="color: red;"><?php echo form_error($captcha['name']); ?></td>
-	</tr>
-	<?php }
-	} ?>
-
 	<tr>
 		<td colspan="3">
 			<?php echo form_checkbox($remember); ?>
