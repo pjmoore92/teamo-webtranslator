34d30b49d49a07e71a1cf916f9004c7b9c5fc918
andrei.mustata@gmail.com <andrei.mustata@gmail.com@bca2885e-21bc-f969-c56a-416bafda769c>
2012-03-18 03:57:53 +0000

    Controllers/auth: removed extra slashes in URLs; made the other auth methods use the main template view

--- trunk/application/views/login_form.php
+++ trunk/application/views/login_form.php
@@ -82,7 +82,7 @@ $captcha = array(
 		<td colspan="3">
 			<?php echo form_checkbox($remember); ?>
 			<?php echo form_label('Remember me', $remember['id']); ?>
-			<?php echo anchor('/auth/forgot_password/', 'Forgot password'); ?>
+			<?php echo anchor('auth/forgot_password/', 'Forgot password'); ?>
 			<?php if ($this->config->item('allow_registration', 'tank_auth')) echo anchor('/auth/register/', 'Register'); ?>
 		</td>
 	</tr>
